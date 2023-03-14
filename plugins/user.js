678ba3cd1cd8a08950b65d5defd496818f5d7e7e
SUHAID-BRO <99546226+SUHAID-BRO@users.noreply.github.com>
2023-03-14 15:53:24 +0530

    Create user.js

---
+++ plugins/user.js
@@ -0,0 +1,90 @@
+const { command } = require("../lib");
+
+/* Copyright (C) 2022 SUHAID-BRO.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+Afiya-md
+*/
+
+command(
+  {
+    pattern: "pp",
+    fromMe: true,
+    type: "user",
+  },
+  async (message, match, m) => {
+    if (!message.reply_message.image)
+      return await message.reply("_Please Reply To A Photo_");
+    let buff = await m.quoted.download();
+    await message.setPP(message.user, buff);
+    return await message.reply("_Profile Picture Updated_");
+  }
+);
+
+command(
+  {
+    pattern: "setname ?(.*)",
+    fromMe: true,
+    type: "user",
+  },
+  async (message, match) => {
+    if (!match) return await message.reply("_Enter name_");
+    await message.updateName(match);
+    return await message.reply(`_Username Updated : ${match}_`);
+  }
+);
+
+command(
+  {
+    pattern: "block ?(.*)",
+    fromMe: true,
+    type: "user",
+  },
+  async (message, match) => {
+    if (message.isGroup) {
+      let jid = message.mention[0] || message.reply_message.jid;
+      if (!jid) return await message.reply("_Reply to a person or mention_");
+      await message.block(jid);
+      return await message.sendMessageMessage(`_@${jid.split("@")[0]} Blocked_`, {
+        mentions: [jid],
+      });
+    } else {
+      await message.block(message.jid);
+      return await message.reply("_User blocked_");
+    }
+  }
+);
+
+command(
+  {
+    pattern: "unblock ?(.*)",
+    fromMe: true,
+    type: "user",
+  },
+  async (message, match) => {
+    if (message.isGroup) {
+      let jid = message.mention[0] || message.reply_message.jid;
+      if (!jid) return await message.reply("_Reply to a person or mention_");
+      await message.block(jid);
+      return await message.sendMessage(`_@${jid.split("@")[0]} unblocked_`, {
+        mentions: [jid],
+      });
+    } else {
+      await message.unblock(message.jid);
+      return await message.reply("_User unblocked_");
+    }
+  }
+);
+
+command(
+  {
+    pattern: "jid ?(.*)",
+    fromMe: true,
+    type: "user",
+  },
+  async (message, match) => {
+    return await message.sendMessage(
+      message.mention[0] || message.reply_message.jid || message.jid
+    );
+  }
+);
